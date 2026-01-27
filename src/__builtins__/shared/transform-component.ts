e06092c73a3a2f94359680b3a6ccb7cd1b0a406b
hezhengxu <hzx.tony@hotmail.com>
2026-01-27 09:24:46 +0800

    refactor: 移除未使用的 transformComponent 函数及相关导入

--- src/__builtins__/shared/transform-component.ts
+++ src/__builtins__/shared/transform-component.ts
@@ -2,31 +2,8 @@ import type { IComponentMapper } from '@silver-formily/vue'
 import type { Component } from 'vue'
 import { isVoidField } from '@formily/core'
 import { observer } from '@formily/reactive-vue'
-import { each } from '@formily/shared'
 import { useField } from '@silver-formily/vue'
 import { defineComponent, h } from 'vue'
-import { useCleanAttrs } from './utils'
-
-type ListenersTransformRules = Record<string, string>
-
-export function transformComponent<T extends Record<string, any>>(tag: any, transformRules?: ListenersTransformRules): Component<T> | any {
-  const componentName = tag.name.split('El')[1]
-  return defineComponent({
-    name: `F${componentName}`,
-    setup(props, { attrs, slots }) {
-      return () => {
-        const { props: data } = useCleanAttrs()
-        if (transformRules) {
-          each(transformRules, (event, extract) => {
-            data.value[`on${event[0].toUpperCase()}${event.slice(1)}`]
-              = attrs[`on${extract[0].toUpperCase()}${extract.slice(1)}`]
-          })
-        }
-        return h(tag, data.value, slots)
-      }
-    },
-  })
-}
 
 // fork from https://github.com/alibaba/formily/blob/7c64c671252adf85471ac5aabfddbaf4fc537354/packages/vue/src/shared/connect.ts#L65
 export function mapReadPretty(
